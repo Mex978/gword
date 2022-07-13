116f574e7740d008f8ea0319d1dfd05d4dcbd61a
patrick <patricklucas524@gmail.com>
2022-07-13 13:50:18 -0300

    Update flavors.dart

--- lib/flavors.dart
+++ lib/flavors.dart
@@ -1,3 +1,5 @@
+import 'package:package_info_plus/package_info_plus.dart';
+
 enum Flavor {
   dev,
   qa,
@@ -5,23 +7,66 @@ enum Flavor {
   prod,
 }
 
-class F {
+enum Environment {
+  dev,
+  qa,
+  hml,
+  prod,
+}
+
+extension FlavorExtension on Flavor {
+  Environment get environment {
+    switch (this) {
+      case Flavor.dev:
+        return Environment.dev;
+      case Flavor.qa:
+        return Environment.qa;
+      case Flavor.hml:
+        return Environment.hml;
+      case Flavor.prod:
+        return Environment.prod;
+      default:
+        return Environment.dev;
+    }
+  }
+}
+
+class EnvironmentFlavor {
   static Flavor? appFlavor;
 
-  static String get name => appFlavor?.name ?? '';
+  static Future<void> fetchFlavor() async {
+    await PackageInfo.fromPlatform().then((PackageInfo packageInfo) {
+      switch (packageInfo.packageName) {
+        case "com.drteladoc.dev":
+          appFlavor = Flavor.dev;
+          break;
+        case "com.drteladoc.qa":
+          appFlavor = Flavor.qa;
+          break;
+        case "com.drteladoc.homolog":
+          appFlavor = Flavor.hml;
+          break;
+        case "com.drteladoc":
+          appFlavor = Flavor.prod;
+          break;
+        default:
+          appFlavor = Flavor.dev;
+      }
+    });
+  }
 
   static String get title {
     switch (appFlavor) {
       case Flavor.dev:
-        return 'your app name to dev';
+        return 'Your app title - DEV';
       case Flavor.qa:
-        return 'your app name to qa';
+        return 'Your app title - QA';
       case Flavor.hml:
-        return 'your app name to hml';
+        return 'Your app title - HOM';
       case Flavor.prod:
-        return 'your app name to prod';
+        return 'Your app title';
       default:
-        return 'title';
+        return 'Your app title';
     }
   }
 }
